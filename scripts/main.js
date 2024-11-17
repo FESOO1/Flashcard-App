3fb1abff577003a33671b603c3feba8aed4a947b
FES001 <firdavs7794948@gmail.com>
2024-11-18 02:04:15 +0500

    Working on it.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
-const flashCardItself = document.querySelector('.flashcard-itself');
+const flashcardItself = document.querySelector('.flashcard-itself');
+const flashcardShadow = document.querySelector('.flashcard-shadow');
 let cardFlipped = false;
 // FLIP THE CARD
 
 function flipTheCard() {
     if (cardFlipped === false) {
-        flashCardItself.classList.add('flashcard-itself-flipped');
+        flashcardItself.classList.add('flashcard-itself-flipped');
+        flashcardShadow.classList.add('flashcard-shadow-flipped');
 
         cardFlipped = true;
     } else {
-        flashCardItself.classList.remove('flashcard-itself-flipped');
+        flashcardItself.classList.remove('flashcard-itself-flipped');
+        flashcardShadow.classList.remove('flashcard-shadow-flipped');
 
         cardFlipped = false;
     };
